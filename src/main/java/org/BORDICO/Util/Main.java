d5965b98bd7082007b939b9089d7f52930618579
Rixio Morales <rixio1994@gmail.com>
2024-08-20 18:29:34 -0400

    Initial Commit

---
+++ src/main/java/org/BORDICO/Util/Main.java
@@ -0,0 +1,10 @@
+package org.BORDICO.Util;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
