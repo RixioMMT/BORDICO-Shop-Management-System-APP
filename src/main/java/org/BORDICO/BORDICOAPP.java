69acdf2d012157ea11156ebe68c24fea02b7cfbb
Rixio Morales <rixio1994@gmail.com>
2024-08-20 20:05:50 -0400

    Initial Commit

---
+++ src/main/java/org/BORDICO/BORDICOAPP.java
@@ -0,0 +1,15 @@
+package org.BORDICO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan(basePackages = "org.BORDICO")
+public class BORDICOAPP {
+    public static void main(String[] args) {
+        SpringApplication.run(BORDICOAPP.class, args);
+    }
+}
