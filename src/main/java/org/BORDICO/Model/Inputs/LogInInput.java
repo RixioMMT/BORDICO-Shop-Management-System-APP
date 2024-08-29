2a35624be9fee112a995ec81957f163401a8ff39
Rixio Morales <rixio1994@gmail.com>
2024-08-29 10:44:02 -0400

    Commit

---
+++ src/main/java/org/BORDICO/Model/Inputs/LogInInput.java
@@ -0,0 +1,11 @@
+package org.BORDICO.Model.Inputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LogInInput {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/org/BORDICO/Service/AuthenticationService.java
@@ -0,0 +1,78 @@
+package org.BORDICO.Service;
+
+import graphql.GraphQLException;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.BORDICO.Model.Entity.Cart;
+import org.BORDICO.Model.Entity.Role;
+import org.BORDICO.Model.Entity.User;
+import org.BORDICO.Model.Enum.RolePosition;
+import org.BORDICO.Model.Inputs.LogInInput;
+import org.BORDICO.Model.Inputs.UserInput;
+import org.BORDICO.Repository.CartRepository;
+import org.BORDICO.Repository.RoleRepository;
+import org.BORDICO.Repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Builder
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final RoleRepository roleRepository;
+    private final CartRepository cartRepository;
+    public User logIn(LogInInput logInInput) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        logInInput.getEmail(),
+                        logInInput.getPassword()
+                )
+        );
+        return userRepository.findByEmail(logInInput.getEmail());
+    }
+    public User signUp(UserInput userInput) throws GraphQLException {
+        if (userInput.getEmail().isBlank() || userInput.getPhone().isBlank() || userInput.getPassword().isBlank()) {
+            throw new GraphQLException("Empty values are not allowed");
+        }
+        String phoneInput = userInput.getPhone().replaceAll("\\D", "");
+        if (phoneInput.isBlank()) {
+            throw new GraphQLException("Phone number must contain at least one digit");
+        }
+        if (userRepository.findByEmail(userInput.getEmail()) != null) {
+            throw new GraphQLException("Email is already used: " + userInput.getEmail());
+        }
+        Set<Role> roles = new HashSet<>();
+        for (RolePosition rolePosition : userInput.getRolePositions()) {
+            Role role = roleRepository.findByRolePosition(rolePosition);
+            if (role == null) {
+                throw new GraphQLException("Role not found: " + rolePosition);
+            }
+            roles.add(role);
+        }
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .phone(phoneInput)
+                .password(passwordEncoder.encode(userInput.getPassword()))
+                .firstName(userInput.getFirstName())
+                .lastName(userInput.getLastName())
+                .roles(roles)
+                .build();
+        user = userRepository.save(user);
+        Set<Cart> carts = new HashSet<>();
+        Cart cart = Cart.builder()
+                .user(user)
+                .build();
+        cart = cartRepository.save(cart);
+        carts.add(cart);
+        user.setCarts(carts);
+        return userRepository.save(user);
+    }
+}
